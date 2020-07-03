9e387385b6e2e56b3654df54136a08f08ed12b50
Sergio Andrés Londono Gallo <Slondonog@intergrupo.com>
2020-07-03 09:19:30 -0500

    20200703. Tarea 16.

--- src/app/pages/hospitales/hospitales.component.ts
+++ src/app/pages/hospitales/hospitales.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ModalUploadService } from "src/app/components/modal-upload/modal-upload.service";
+import { HospitalService } from "src/app/services/service.index";
+import { Hospital } from "src/app/models/hospital.model";
 
 @Component({
-  selector: 'app-hospitales',
-  templateUrl: './hospitales.component.html',
-  styleUrls: ['./hospitales.component.css']
+  selector: "app-hospitales",
+  templateUrl: "./hospitales.component.html",
+  styleUrls: ["./hospitales.component.css"],
 })
 export class HospitalesComponent implements OnInit {
+  hospitales: Hospital[] = [];
+  desde = 0;
+  totalRegistros = 0;
+  cargando = true;
 
-  constructor() { }
+  constructor(
+    public _hospitalService: HospitalService,
+    public _modalUploadService: ModalUploadService
+  ) {
+    this.cargarHospitales();
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  cargarHospitales() {
+    this.cargando = true;
+
+    this._hospitalService.cargarHospitales().subscribe((resp: any) => {
+      this.totalRegistros = resp.total;
+      this.hospitales = resp.hospitales;
+      this.cargando = false;
+    });
+  }
+
+  buscarHospital(termino: string) {
+    if (termino.length <= 0) {
+      this.cargarHospitales();
+      return;
+    }
+
+    this.cargando = true;
+
+    this._hospitalService
+      .buscarUsuarios(termino)
+      .subscribe((hospitales: Hospital[]) => {
+        this.hospitales = hospitales;
+        this.cargando = false;
+        console.log(this.hospitales);
+      });
   }
 
 }
--- src/app/services/hospital/hospital.service.ts
+++ src/app/services/hospital/hospital.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { SubirArchivoService } from '../service.index';
+import { URL_SERVICIOS } from 'src/app/config/config';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HospitalService {
 
-  constructor() { }
+  baseUrl: string = URL_SERVICIOS;
+
+  constructor(public http: HttpClient,
+              public router: Router,
+              public _subirArchivoService: SubirArchivoService) { }
+
+  cargarHospitales() {
+    const url = this.baseUrl + '/hospital';
+
+    return this.http.get(url);
+  }
+
+  buscarUsuarios(termino: string) {
+    const url = this.baseUrl + '/busqueda/coleccion/hospitales/' + termino;
+    return this.http.get(url).pipe(map((resp: any) => resp.hospitales));
+  }
+
 }
